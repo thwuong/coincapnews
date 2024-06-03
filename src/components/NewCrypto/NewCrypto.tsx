1e177f89020b7571f951fe410208df35b494fc6b
tuyen1204 <61371510+tuyen1204@users.noreply.github.com>
2024-06-03 15:57:30 +0700

    [update]: className font

--- src/components/NewCrypto/NewCrypto.tsx
+++ src/components/NewCrypto/NewCrypto.tsx
@@ -207,145 +207,145 @@ function NewCryptoTable({
                 <Tbody>
                     {!isLoading
                         ? table.getRowModel().rows.map((row) => {
-                              return (
-                                  <Tr key={row.index}>
-                                      <Td
-                                          px={"8px"}
-                                          position={width <= 768 ? "sticky" : undefined}
-                                          left={0}
-                                          className="bg-secondary"
-                                          textAlign={"center"}
-                                          fontWeight={"500"}
-                                      >
-                                          {row.index + 1 + currentIndex}
-                                      </Td>
-                                      <Td
-                                          px={"4px"}
-                                          minW={"104px"}
-                                          position={width <= 768 ? "sticky" : undefined}
-                                          left={8}
-                                          className="bg-secondary"
-                                      >
-                                          <Link
-                                              href={`/currency/${row.original.id}`}
-                                              className="flex items-center gap-3"
-                                          >
-                                              {checkFormatImage(row.original.image) && (
-                                                  <Image
-                                                      src={row.original.image}
-                                                      alt={row.original.name}
-                                                      width={24}
-                                                      height={24}
-                                                  />
-                                              )}
+                            return (
+                                <Tr key={row.index}>
+                                    <Td
+                                        px={"8px"}
+                                        position={width <= 768 ? "sticky" : undefined}
+                                        left={0}
+                                        className="bg-secondary text-sm"
+                                        textAlign={"center"}
+                                        fontWeight={"500"}
+                                    >
+                                        {row.index + 1 + currentIndex}
+                                    </Td>
+                                    <Td
+                                        px={"4px"}
+                                        minW={"104px"}
+                                        position={width <= 768 ? "sticky" : undefined}
+                                        left={8}
+                                        className="bg-secondary"
+                                    >
+                                        <Link
+                                            href={`/currency/${row.original.id}`}
+                                            className="flex items-center gap-3"
+                                        >
+                                            {checkFormatImage(row.original.image) && (
+                                                <Image
+                                                    src={row.original.image}
+                                                    alt={row.original.name}
+                                                    width={24}
+                                                    height={24}
+                                                />
+                                            )}
 
-                                              <p className="capitalize text-sm leading-4 font-semibold text-typo-4 ">
-                                                  {row.original.name}
-                                              </p>
-                                          </Link>
-                                      </Td>
-                                      <Td px={"4px"}>
-                                          <p className="text-center text-sm leading-4 font-medium text-typo-4 ">
-                                              {formatCurrency(row.original.current_price, "USD", currentLanguage, {
-                                                  maximumFractionDigits: 9,
-                                              })}
-                                          </p>
-                                      </Td>
-                                      <Td px={"4px"}>
-                                          <p
-                                              className={clsx(
-                                                  "capitalize text-sm text-center leading-4 change24 font-medium ",
-                                                  row.original.price_change_percentage_1h_in_currency > 0
-                                                      ? "text-up"
-                                                      : "text-down"
-                                              )}
-                                          >
-                                              {row.original.price_change_percentage_1h_in_currency?.toFixed(2) || 0}%
-                                          </p>
-                                      </Td>
-                                      <Td px={"4px"}>
-                                          <p
-                                              className={clsx(
-                                                  "capitalize text-sm text-center leading-4 change24 font-medium ",
-                                                  row.original.price_change_percentage_24h_in_currency > 0
-                                                      ? "text-up"
-                                                      : "text-down"
-                                              )}
-                                          >
-                                              {row.original.price_change_percentage_24h_in_currency?.toFixed(2) || 0}%
-                                          </p>
-                                      </Td>
-                                      <Td px={"4px"}>
-                                          <p className="capitalize text-center text-sm leading-4 font-medium text-typo-1 ">
-                                              {formatCurrency(row.original.market_cap)}
-                                          </p>
-                                      </Td>
-                                      <Td px={"4px"}>
-                                          <p className="capitalize text-center text-sm leading-4 font-medium text-typo-1 ">
-                                              {formatCurrency(row.original.volume_24h || 0)}
-                                          </p>
-                                      </Td>
-                                      <Td px={"4px"}>
-                                          <p className="capitalize text-center text-sm leading-4 font-medium text-typo-1 ">
-                                              {/* {row.original.blockchain} */}
-                                              Blockchanin
-                                          </p>
-                                      </Td>
-                                      <Td px={"4px"}>
-                                          <p className="capitalize text-center text-sm leading-4 font-medium text-typo-1 ">
-                                              {moment(row.original.atl_date).fromNow()}
-                                          </p>
-                                      </Td>
-                                  </Tr>
-                              );
-                          })
+                                            <p className="capitalize text-sm leading-4 font-semibold text-typo-4 ">
+                                                {row.original.name}
+                                            </p>
+                                        </Link>
+                                    </Td>
+                                    <Td px={"4px"}>
+                                        <p className="text-center text-sm leading-4 font-medium text-typo-4 ">
+                                            {formatCurrency(row.original.current_price, "USD", currentLanguage, {
+                                                maximumFractionDigits: 9,
+                                            })}
+                                        </p>
+                                    </Td>
+                                    <Td px={"4px"}>
+                                        <p
+                                            className={clsx(
+                                                "capitalize text-sm text-center leading-4 change24 font-medium ",
+                                                row.original.price_change_percentage_1h_in_currency > 0
+                                                    ? "text-up"
+                                                    : "text-down"
+                                            )}
+                                        >
+                                            {row.original.price_change_percentage_1h_in_currency?.toFixed(2) || 0}%
+                                        </p>
+                                    </Td>
+                                    <Td px={"4px"}>
+                                        <p
+                                            className={clsx(
+                                                "capitalize text-sm text-center leading-4 change24 font-medium ",
+                                                row.original.price_change_percentage_24h_in_currency > 0
+                                                    ? "text-up"
+                                                    : "text-down"
+                                            )}
+                                        >
+                                            {row.original.price_change_percentage_24h_in_currency?.toFixed(2) || 0}%
+                                        </p>
+                                    </Td>
+                                    <Td px={"4px"}>
+                                        <p className="capitalize text-center text-sm leading-4 font-medium text-typo-1 ">
+                                            {formatCurrency(row.original.market_cap)}
+                                        </p>
+                                    </Td>
+                                    <Td px={"4px"}>
+                                        <p className="capitalize text-center text-sm leading-4 font-medium text-typo-1 ">
+                                            {formatCurrency(row.original.volume_24h || 0)}
+                                        </p>
+                                    </Td>
+                                    <Td px={"4px"}>
+                                        <p className="capitalize text-center text-sm leading-4 font-medium text-typo-1 ">
+                                            {/* {row.original.blockchain} */}
+                                            Blockchanin
+                                        </p>
+                                    </Td>
+                                    <Td px={"4px"}>
+                                        <p className="capitalize text-center text-sm leading-4 font-medium text-typo-1 ">
+                                            {moment(row.original.atl_date).fromNow()}
+                                        </p>
+                                    </Td>
+                                </Tr>
+                            );
+                        })
                         : Array(10)
-                              .fill(0)
-                              .map((_, index) => {
-                                  return (
-                                      <Tr key={index}>
-                                          <Td isNumeric={true} px={"8px"} py={"16px"} minW={"48px"}>
-                                              <SkeletonText noOfLines={1} spacing="4" skeletonHeight="2" />
-                                          </Td>
-                                          <Td
-                                              p={"4px"}
-                                              height={"57px"}
-                                              minW={"350px"}
-                                              position={width <= 768 ? "sticky" : undefined}
-                                              className="bg-secondary"
-                                          >
-                                              <div className="flex items-center gap-4">
-                                                  <SkeletonCircle size="5" />
-                                                  <Skeleton
-                                                      height={"8px"}
-                                                      width={`${Math.floor(Math.random() * 31) + 50}%`}
-                                                  />
-                                              </div>
-                                          </Td>
-                                          <Td isNumeric={true} px={"4px"} minW={"166px"}>
-                                              <SkeletonText noOfLines={1} spacing="2" skeletonHeight="2" />
-                                          </Td>
-                                          <Td isNumeric={true} px={"4px"} minW={"75px"}>
-                                              <SkeletonText noOfLines={1} spacing="2" skeletonHeight="2" />
-                                          </Td>
-                                          <Td isNumeric={true} px={"4px"} minW={"88px"}>
-                                              <SkeletonText noOfLines={1} spacing="2" skeletonHeight="2" />
-                                          </Td>
-                                          <Td isNumeric={true} px={"4px"} minW={"180px"}>
-                                              <SkeletonText noOfLines={1} spacing="2" skeletonHeight="2" />
-                                          </Td>
-                                          <Td isNumeric={true} px={"4px"} minW={"180px"}>
-                                              <SkeletonText noOfLines={1} spacing="2" skeletonHeight="2" />
-                                          </Td>
-                                          <Td isNumeric={true} px={"4px"} minW={"117px"}>
-                                              <SkeletonText noOfLines={1} spacing="2" skeletonHeight="2" />
-                                          </Td>
-                                          <Td isNumeric={true} px={"4px"} minW={"133px"}>
-                                              <SkeletonText noOfLines={1} spacing="2" skeletonHeight="2" />
-                                          </Td>
-                                      </Tr>
-                                  );
-                              })}
+                            .fill(0)
+                            .map((_, index) => {
+                                return (
+                                    <Tr key={index}>
+                                        <Td isNumeric={true} px={"8px"} py={"16px"} minW={"48px"}>
+                                            <SkeletonText noOfLines={1} spacing="4" skeletonHeight="2" />
+                                        </Td>
+                                        <Td
+                                            p={"4px"}
+                                            height={"57px"}
+                                            minW={"350px"}
+                                            position={width <= 768 ? "sticky" : undefined}
+                                            className="bg-secondary"
+                                        >
+                                            <div className="flex items-center gap-4">
+                                                <SkeletonCircle size="5" />
+                                                <Skeleton
+                                                    height={"8px"}
+                                                    width={`${Math.floor(Math.random() * 31) + 50}%`}
+                                                />
+                                            </div>
+                                        </Td>
+                                        <Td isNumeric={true} px={"4px"} minW={"166px"}>
+                                            <SkeletonText noOfLines={1} spacing="2" skeletonHeight="2" />
+                                        </Td>
+                                        <Td isNumeric={true} px={"4px"} minW={"75px"}>
+                                            <SkeletonText noOfLines={1} spacing="2" skeletonHeight="2" />
+                                        </Td>
+                                        <Td isNumeric={true} px={"4px"} minW={"88px"}>
+                                            <SkeletonText noOfLines={1} spacing="2" skeletonHeight="2" />
+                                        </Td>
+                                        <Td isNumeric={true} px={"4px"} minW={"180px"}>
+                                            <SkeletonText noOfLines={1} spacing="2" skeletonHeight="2" />
+                                        </Td>
+                                        <Td isNumeric={true} px={"4px"} minW={"180px"}>
+                                            <SkeletonText noOfLines={1} spacing="2" skeletonHeight="2" />
+                                        </Td>
+                                        <Td isNumeric={true} px={"4px"} minW={"117px"}>
+                                            <SkeletonText noOfLines={1} spacing="2" skeletonHeight="2" />
+                                        </Td>
+                                        <Td isNumeric={true} px={"4px"} minW={"133px"}>
+                                            <SkeletonText noOfLines={1} spacing="2" skeletonHeight="2" />
+                                        </Td>
+                                    </Tr>
+                                );
+                            })}
                 </Tbody>
             </Table>
         </TableContainer>
